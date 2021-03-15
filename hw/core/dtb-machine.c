e588f7d7de6fe844fc1c5849a1344eb2e738404a
Alfred Wanga <awanga@gmail.com>
2021-03-15 11:20:19 -0400

    dtb-parser: add dynamically configured machine based on dtb at runtime
    The dtb-parser is an architecture-independent machine that is dynamically
    configured by reading the device tree binary provided to QEMU at runtime.
    
    Its intended use is for emulation of devices that often have commonly used
    hardware IP, but vary widely in hardware implementation (e.g. ARM embedded
    SoCs). This machine would allow for at least partial emulation without the need
    to write machine code for a (complex) new device, with the downside of the
    automatically generated machine generating a sub-optimal or partially incorrect
    model of the device and the initial generation of the machine at runtime.
    
    Because the device tree binary can be implemented in many ways, this machine is
    designed to be as generic as possible, while making use of some QEMU and
    machine-specific overrides when the device tree is not explicit and/or
    sufficient. The intention is that device trees developed for other purposes
    (bootloader/linux, auto-generated from vendor tools, etc.) can be used without
    modification to speed debug and prototyping.
    
    v0 - 3/15/21
    Development of this patch is ongoing, but it currently parses the dtb passed to
    QEMU and categorizes relevant devices at runtime. No working code to instantiate
    devices or busses is yet written.

---
+++ hw/core/dtb-machine.c
@@ -0,0 +1,544 @@
+/*
+ * DTB-Parsed Machine
+ *
+ * Author: Alfred Wanga <awanga@gmail.com>
+ *
+ * Portions written by Benjamin Fair
+ *
+ * This work is licensed under the terms of the GNU GPL, version 2 or later.
+ * See the COPYING file in the top-level directory.
+ *
+ */
+
+#include "qemu/osdep.h"
+#include "qapi/error.h"
+#include "qemu/error-report.h"
+#include "qemu/log.h"
+#include "qemu/option.h"
+#include "hw/boards.h"
+#include "hw/qdev-properties.h"
+#include "hw/loader.h"
+#include "exec/address-spaces.h"
+#include "sysemu/sysemu.h"
+#include "sysemu/device_tree.h"
+
+#include "hw/core/cpu.h"
+#include "hw/dtb-parse/dtb-parse.h"
+#include "migration/vmstate.h"
+
+static const char DTB_CPU_NODE[] = "cpus";
+static const char DTB_MEM_NODE[] = "memory";
+
+static const char DTB_PROP_COMPAT[] = "compatible";
+
+typedef struct {
+    MachineState *mch;
+    MemoryRegion *ram;
+    CPUState *cpu[DTB_PARSE_MAX_NUM_CPUS];
+
+    const char *model_name;
+    unsigned ncpus;
+
+    /* TODO: this may really want to be a hash, not a linked list */
+    struct device_fdt_mapping {
+        struct device_fdt_mapping *next;
+        DeviceState *dev;
+        enum fdt_node_type {
+            NODE_TYPE_UNKNOWN    = 0,
+            NODE_TYPE_BUS,
+            NODE_TYPE_DEVICE,
+            NODE_TYPE_CLOCK,
+        } dev_type;
+        int offset;
+    } *mapping;
+
+} DynamicState;
+
+/* from libfdt code written by Benjamin Fair */
+static int _fdt_read_cells(const fdt32_t *cells, unsigned int n,
+    uint64_t *value)
+{
+    int i;
+
+    if (n > 2) {
+        return -FDT_ERR_BADNCELLS;
+    }
+
+    *value = 0;
+    for (i = 0; i < n; i++) {
+        *value <<= (sizeof(*cells) * 8);
+        *value |= fdt32_to_cpu(cells[i]);
+    }
+
+    return 0;
+}
+
+/* from libfdt code written by Benjamin Fair */
+int fdt_simple_addr_size(const void *fdt, int nodeoffset, int idx,
+         uint64_t *addrp, uint64_t *sizep)
+{
+    int parent;
+    int ac, sc, reg_stride;
+    int res;
+    const fdt32_t *reg;
+
+    reg = fdt_getprop(fdt, nodeoffset, "reg", &res);
+    if (res < 0) {
+        return res;
+    }
+
+    parent = fdt_parent_offset(fdt, nodeoffset);
+    if (parent == -FDT_ERR_NOTFOUND) {
+        /*
+         * a node without a parent does
+         * not have _any_ number of cells
+         */
+        return -FDT_ERR_BADNCELLS;
+    }
+    if (parent < 0) {
+        return parent;
+    }
+
+    ac = fdt_address_cells(fdt, parent);
+    if (ac < 0) {
+        return ac;
+    }
+
+    sc = fdt_size_cells(fdt, parent);
+    if (sc < 0) {
+        return sc;
+    }
+
+    reg_stride = ac + sc;
+
+    /*
+     * res is the number of bytes read and must be an even multiple of the
+     * sum of address cells and size cells
+     */
+    if ((res % (reg_stride * sizeof(*reg))) != 0) {
+        return -FDT_ERR_BADVALUE;
+    }
+
+    if (addrp) {
+        res = _fdt_read_cells(&reg[reg_stride * idx], ac, addrp);
+        if (res < 0) {
+            return res;
+        }
+    }
+    if (sizep) {
+        res = _fdt_read_cells(&reg[ac + reg_stride * idx], sc, sizep);
+        if (res < 0) {
+            return res;
+        }
+    }
+
+    return 0;
+}
+
+static int add_dev_fdt_mapping(DynamicState *state, DeviceState *dev,
+                                      int node_offset)
+{
+    struct device_fdt_mapping *node =
+                g_malloc0(sizeof(struct device_fdt_mapping));
+
+    node->dev = dev;
+    node->offset = node_offset;
+    node->next = state->mapping;
+    state->mapping = node;
+    return 0;
+}
+
+static struct DeviceState *find_dev_fdt_mapping(
+                                    DynamicState *state, int node)
+{
+    struct device_fdt_mapping *head = state->mapping;
+
+    while (head != NULL) {
+        if (head->offset == node) {
+            return head->dev;
+        }
+        head = head->next;
+    }
+    return NULL;
+}
+
+static DeviceState *machine_dtb_add_clocksource(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add pci bus code here */
+    pr_debug("adding %s as clocksource", fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_pci_bus(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add pci bus code here */
+    pr_debug("adding %s as pci bus", fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_peripheral_bus(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add i2c/spi bus code here */
+    pr_debug("adding %s as periph bus", fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_generic_bus(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add generic bus code here */
+    pr_debug("adding %s as generic bus", fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_intr_controller(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add interrupt controller code here */
+    pr_debug("adding %s as interrupt controller",
+                    fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_gpio_controller(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add gpio controller code here */
+    pr_debug("adding %s as gpio controller", fdt_get_name(fdt, node, NULL));
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_simple_device(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    pr_debug("adding %s as simple device", fdt_get_name(fdt, node, NULL));
+    uint64_t reg_addr, reg_size;
+    unsigned i, compat_num;
+
+    /* try to instantiate "regular" device node using compatible string */
+    compat_num = fdt_stringlist_count(fdt, node, DTB_PROP_COMPAT);
+    for (i = 0; i < compat_num; i++) {
+        const char *compat = fdt_stringlist_get(fdt, node,
+                                DTB_PROP_COMPAT, i, NULL);
+
+        /* strip manufacturer and try to create new device */
+        /*
+         * dev = qdev_try_new(str_fdt_compat_strip(compat));
+         * if (dev) {
+         *     pr_debug("creating device %s", str_fdt_compat_strip(compat));
+         *     sysbus_realize_and_unref(SYS_BUS_DEVICE(dev), &error_fatal);
+         *     break;
+         * }
+         */
+    }
+
+    if (!dev) {
+        return NULL;
+    }
+
+    /* add memory region when reg property exists */
+    fdt_for_each_reg_prop(i, fdt, node, &reg_addr, &reg_size) {
+        pr_debug("adding mmio region %llx", reg_addr);
+        sysbus_mmio_map(SYS_BUS_DEVICE(dev), 0, reg_addr);
+    }
+
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_dummy_device(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    /* TODO: add dummy device code here */
+    return dev;
+}
+
+static DeviceState *machine_dtb_add_device_node(DynamicState *state,
+                DeviceState *parent_dev, const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    bool has_subnodes = fdt_first_subnode(fdt, node) > 0;
+    const char *node_name = fdt_get_name(fdt, node, NULL);
+    const char *dev_type = (const char *)fdt_getprop(fdt, node,
+                                                     "device_type", NULL);
+
+    /* check for explicit bus device types */
+    if (dev_type != NULL) {
+
+        /* check for pci bus */
+        if (strncmp(dev_type, "pci", 3) == 0) {
+            dev = machine_dtb_add_pci_bus(state, parent_dev, fdt, node);
+            goto done;
+        }
+
+        /* check for soc (generic) busses */
+        if (strncmp(dev_type, "soc", 3) == 0) {
+            dev = machine_dtb_add_generic_bus(state, parent_dev, fdt, node);
+            goto done;
+        }
+    }
+
+    /* check if node is a bus */
+    if (has_subnodes) {
+        unsigned i, compat_num;
+        bool has_ranges = (fdt_getprop(fdt, node, "ranges", NULL) != NULL);
+
+        /* check for generic busses */
+        if (has_ranges) {
+            dev = machine_dtb_add_generic_bus(state, parent_dev, fdt, node);
+            goto done;
+        }
+
+        /* check for i2c/spi bus */
+        compat_num = fdt_stringlist_count(fdt, node, DTB_PROP_COMPAT);
+        for (i = 0; i < compat_num; i++) {
+            const char *compat = fdt_stringlist_get(fdt, node,
+                                                DTB_PROP_COMPAT, i, NULL);
+
+            /* FIXME: need more foolproof way to detect peripheral bus */
+            if ((strstr(compat, "i2c") != NULL &&
+                 strstr(node_name, "i2c") != NULL) ||
+                (strstr(compat, "spi") != NULL &&
+                 strstr(node_name, "spi") != NULL)) {
+                dev = machine_dtb_add_peripheral_bus(state,
+                                        parent_dev, fdt, node);
+                goto done;
+            }
+        }
+    }
+
+    /* check for gpio/interrupt controller device (must check gpio first!) */
+    if (fdt_getprop(fdt, node, "gpio-controller", NULL) > 0) {
+        dev = machine_dtb_add_gpio_controller(state, parent_dev, fdt, node);
+        goto done;
+    }
+    if (fdt_getprop(fdt, node, "interrupt-controller", NULL) > 0) {
+        dev = machine_dtb_add_intr_controller(state, parent_dev, fdt, node);
+        goto done;
+    }
+
+    /* special case: check for "clock-cells" property */
+    if (fdt_getprop(fdt, node, "#clock-cells", NULL) > 0) {
+        dev = machine_dtb_add_clocksource(state, parent_dev, fdt, node);
+        goto done;
+    }
+
+    /* try to instantiate "regular" device node using compatible string */
+    if (!dev) {
+        dev = machine_dtb_add_simple_device(state, parent_dev, fdt, node);
+    }
+
+    if (!dev) {
+        /* fallback: try to create dummy device */
+        dev = machine_dtb_add_dummy_device(state, parent_dev, fdt, node);
+        if (!dev) {
+            pr_debug("No device created for node %s", node_name);
+            return NULL;
+        }
+    }
+
+done:
+    add_dev_fdt_mapping(state, dev, node);
+    return dev;
+}
+
+static int machine_dtb_scan_node(DynamicState *state, DeviceState *parent,
+                                 const void *fdt, int node)
+{
+    DeviceState *dev = NULL;
+    int cnt, subnode;
+
+    /* sanity check: has this node been scanned already? */
+    if (find_dev_fdt_mapping(state, node) != NULL) {
+        /* TODO: ensure connectivity to parent device */
+        return 0;
+    }
+
+    /* check for compatible property list */
+    cnt = fdt_stringlist_count(fdt, node, DTB_PROP_COMPAT);
+    if (cnt > 0) {
+
+        /* add device to machine */
+        dev = machine_dtb_add_device_node(state, parent, fdt, node);
+    }
+
+    fdt_for_each_subnode(subnode, fdt, node) {
+        const char *node_name = fdt_get_name(fdt, subnode, NULL);
+
+        /* recursively search subnodes */
+        machine_dtb_scan_node(state, dev, fdt,
+                fdt_subnode_offset(fdt, node, node_name));
+    }
+
+    return 0;
+}
+
+static int machine_load_device_tree(const char *dtb_filename, void **fdt_ptr)
+{
+    void *fdt = NULL;
+    int fdt_size;
+
+    fdt = load_device_tree(dtb_filename, &fdt_size);
+    if (!fdt) {
+        error_report("Error while loading device tree file '%s'",
+            dtb_filename);
+        return -1;
+    }
+
+    g_assert(fdt_ptr);
+    *fdt_ptr = fdt;
+    return 0;
+}
+
+static void machine_dtb_parse_init(MachineState *mch)
+{
+    DynamicState *state = g_new(DynamicState, 1);
+    MemoryRegion *ram = g_new(MemoryRegion, 1);
+    Error *err = NULL;
+    void *fdt = NULL;
+    uint64_t reg_addr, reg_size;
+    int len, offset, cpu_node;
+
+    state->mapping = NULL;
+    state->mch = mch;
+    state->ram = ram;
+
+    /* load the device tree with some checking */
+    if (!mch->dtb) {
+            error_report("DTB Parser machine requires use of -dtb parameter");
+            exit(1);
+    }
+    if (machine_load_device_tree(mch->dtb, &fdt)) {
+            error_report("Cannot locate device tree file!");
+            exit(1);
+    }
+
+    /* get model name from dtb */
+    state->model_name =
+        g_strdup((const char *)qemu_fdt_getprop(fdt, "/", "model", &len, &err));
+    pr_debug("Scanning Device Tree for %s...\n", state->model_name);
+
+    /* identify and init the cpu(s) */
+    state->ncpus = 0;
+
+    /* look for cpu node in device tree */
+    cpu_node = fdt_subnode_offset(fdt, 0, DTB_CPU_NODE);
+    if (cpu_node < 0) {
+        if (!mch->cpu_type) {
+            error_report("Device tree has no CPU node. "
+                         "Use -cpu to manually determine CPU");
+            exit(1);
+        }
+
+        /* no CPU node found - try to use information passed to QEMU */
+        state->cpu[0] = cpu_create(mch->cpu_type);
+        if (!state->cpu[0]) {
+            error_report("No CPU node found. Could not manually init CPU %s",
+                    mch->cpu_type);
+            exit(1);
+        }
+
+        /*
+         * FIXME: when cpus node is missing, typically single CPU topology.
+         *        Need to decide if supporting manually setting multi-core
+         *        without cpus node use case is worth implementing
+         */
+        pr_debug("No CPU node found. Setting CPU to %s", mch->cpu_type);
+        state->ncpus++;
+    } else {
+        /* scan for cpus */
+        fdt_for_each_subnode(offset, fdt, cpu_node) {
+            const char *cpu_path, *cpu_type;
+            uint32_t freq;
+
+            cpu_path = fdt_get_name(fdt, offset, NULL);
+            cpu_type = (const char *)qemu_fdt_getprop(fdt, cpu_path,
+                                        DTB_PROP_COMPAT, &len, &error_fatal);
+
+            pr_debug("Found CPU %s (%s)", cpu_path, cpu_type);
+
+            freq = qemu_fdt_getprop_cell(fdt, cpu_path,
+                        "timebase-frequency", &len, &err);
+            if (len < 0) {
+                /*
+                 * FIXME: create machine property to pass default
+                 * cpu frequency instead of hardcoding value
+                 */
+                freq = 200000000;
+                pr_debug("No frequency found. Default to %dMHz",
+                        freq / 1000000);
+            }
+
+            /* create cpu using compatible string */
+            state->cpu[state->ncpus] = cpu_create(cpu_type);
+            if (!state->cpu[state->ncpus]) {
+                /* try stripping manufacturer from cpu type */
+                const char *cpu_type_model = str_fdt_compat_strip(cpu_type);
+
+                if (cpu_type_model) {
+                    state->cpu[state->ncpus] = cpu_create(cpu_type_model);
+                }
+
+                if (!cpu_type_model || !state->cpu[state->ncpus]) {
+                    error_report("Unable to initialize CPU");
+                    exit(1);
+                }
+            }
+            state->ncpus++;
+        }
+    }
+
+    /* get system memory size */
+    fdt_simple_addr_size(fdt, fdt_subnode_offset(fdt, 0, DTB_MEM_NODE),
+        0, &reg_addr, &reg_size);
+    pr_debug("System Memory = %lluMB @ 0x%llx",
+                    reg_size / 1024 / 1024, reg_addr);
+    mch->ram_size = reg_size;
+    if (!mch->ram_size) {
+        error_report("No memory subnode in device tree found");
+        exit(1);
+    }
+
+    /* register RAM */
+    memory_region_init_ram(ram, NULL, "ram", mch->ram_size, &error_fatal);
+    memory_region_add_subregion(get_system_memory(), reg_addr, ram);
+
+    /* iterate through all root nodes recursively */
+    fdt_for_each_subnode(offset, fdt, 0) {
+        const char *node_name = fdt_get_name(fdt, offset, NULL);
+
+        if (strcmp(node_name, DTB_CPU_NODE) == 0 ||
+            strcmp(node_name, DTB_MEM_NODE) == 0)
+            continue;
+
+        machine_dtb_scan_node(state, NULL, fdt,
+            fdt_subnode_offset(fdt, 0, node_name));
+    }
+
+    /*
+     * TODO: connectivity fixup code after devices are created
+     *       including clocks, interrupts, gpio, etc.
+     */
+
+    error_report("Completed init. Exiting...");
+    exit(1);
+}
+
+static void machine_dtb_parse_machine_init(MachineClass *mc)
+{
+    mc->desc = "device tree parsed machine";
+    mc->init = machine_dtb_parse_init;
+    /*mc->max_cpus = DTB_PARSE_MAX_NUM_CPUS;*/
+}
+
+DEFINE_MACHINE("dtb_parse", machine_dtb_parse_machine_init)
